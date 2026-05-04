d544d3cdca4d135cf998c8a9bace06f3878f6255
mahak-20 <mahakkhurana20@gmail.com>
2026-05-04 21:06:16 +0530

    Add files via upload

---
+++ 60 Days Coding Challenge/day1_basics.py
@@ -0,0 +1,9 @@
+name = input("Enter your name:")
+age = input("Enter your age:")
+
+age = int(age)
+print(f"Hello {name}, you are {age} years old.")
+
+future_age = age + 5
+
+print(f"After 5 years, you will be {future_age} years old.")
