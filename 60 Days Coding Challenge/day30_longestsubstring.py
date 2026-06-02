7a8e9acc8a8a8389a3f62518f523e36ad206cdb4
mahak-20 <mahakkhurana20@gmail.com>
2026-06-02 17:16:28 +0530

    Add files via upload

---
+++ 60 Days Coding Challenge/day30_longestsubstring.py
@@ -0,0 +1,17 @@
+def longest_unique(s):
+   seen = set()
+   left = 0
+   max_len = 0
+   for right in range(len(s)):
+        while s[right] in seen:
+            seen.remove(s[left])
+            left +=1
+        seen.add(s[right])
+
+        max_len = max(
+            max_len, 
+            right - left + 1)
+   return max_len
+   
+s = input("Enter a string: ")
+print(longest_unique(s))
