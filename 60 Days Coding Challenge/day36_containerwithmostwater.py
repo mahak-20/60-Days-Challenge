2d5d004cbf219c9fa73a9886dcfbd59c6ee34fdc
mahak-20 <mahakkhurana20@gmail.com>
2026-06-08 20:39:13 +0530

    Add files via upload

---
+++ 60 Days Coding Challenge/day36_containerwithmostwater.py
@@ -0,0 +1,15 @@
+def max_water(height):
+    left = 0
+    right = len(height) -1
+    max_area = 0
+    while left<right:
+        width = right - left
+        area = width * min(height[left], height[right])
+        max_area = max(max_area, area)
+        if height[left] < height[right]:
+            left += 1
+        else:
+            right -=1
+    return max_area
+height = list(map(int, input("Enter the heights: ").split()))
+print(max_water(height))
